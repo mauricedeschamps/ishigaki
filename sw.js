378c4f46979a274c71a9ac35eb4305376408fbb3
mauricedeschamps <mementomorikatsu@gmail.com>
2026-05-05 21:23:11 +0900

    sw.js の更新

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'island-guide-v1';
+const CACHE_NAME = 'island-guide-v2';
 const urlsToCache = [
   '/',
   'index.html',
@@ -9,15 +9,22 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).then(fetchRes => {
+        return caches.open(CACHE_NAME).then(cache => {
+          if (event.request.url.startsWith('http') && event.request.method === 'GET') {
+            cache.put(event.request, fetchRes.clone());
+          }
+          return fetchRes;
+        });
+      });
+    }).catch(() => caches.match('./index.html'))
   );
 });
 
